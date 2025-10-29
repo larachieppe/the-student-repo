03674bf60cda6d85a866cdad8a1a78c470d7c916
Lara Chieppe <larachieppe@berkeley.edu>
2025-10-28 19:33:26 -0700

    changes to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,34 @@
-import logo from "../assets/logo.png";
-
 export default function Footer() {
   return (
     <footer className="border-t border-brand-line py-10 bg-brand-blue">
-      <div className="container-tight flex flex-col gap-6 md:flex-row md:items-center md:justify-between">
-        <div className="flex items-center gap-2">
-          <span className="inline-flex h-10 w-10 items-center justify-center rounded-md bg-brand-bg p-2">
-            <img src={logo} alt="Logo" />
-          </span>
-          <span className="font-mono font-semibold">Reach Capital</span>
+      <div className="mx-4 flex flex-col gap-6 md:flex-row md:items-start md:justify-between mb-10">
+        <div className="flex items-center text-white">
+          <div className="">
+            <p className="text-white font-semibold">
+              The Repo by Reach Capital
+            </p>
+            <p className="text-white">
+              A live, opt-in talent list for builders, hackers, designers and
+              engineers.
+            </p>
+          </div>
         </div>
-        <div className="text-sm text-white">
-          © {new Date().getFullYear()} Reach Capital. All rights reserved.
+        <div className="">
+          <p className="text-white font-semibold">Resources</p>
+          <p className="text-white">Privacy Policy</p>
+          <p className="text-white">Terms of Service</p>
+          <p className="text-white">Contact Us</p>
         </div>
+        <div className="">
+          <p className=" text-white font-semibold">Connect</p>
+          <p className="text-white">
+            Questions? Reach out to us at hi@repo.reachcapital.com
+          </p>
+        </div>
+      </div>
+      <div className="border-t border-white mx-4" />
+      <div className="text-sm text-white text-center mt-4">
+        © {new Date().getFullYear()} Reach Capital. All rights reserved.
       </div>
     </footer>
   );
