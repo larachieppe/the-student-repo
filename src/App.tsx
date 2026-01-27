f9c4529a84c3a711171f9df09d078f34e96dbcc1
Sodbayar-ganbat <sodbayarg@berkeley.edu>
2026-01-26 19:38:34 -0800

    fixed student portal showing up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,60 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
+import HomePage from "./pages/HomePage";
+import FormPage from "./pages/FormPage";
+import LoginPage from "./pages/LoginPage";
+import SubmittedPage from "./pages/SubmittedPage";
+import AuthCallback from "./AuthCallback";
+import BusinessPortal from "./pages/BusinessPortal";
+import RCPortal from "./pages/RCPortal";
+import RequireAuth from "./components/RequireAuth";
+import PortalLayout from "./components/PortalLayout";
 import StudentPortal from "./pages/StudentPortal";
 
 export default function App() {
   return (
     <>
-      <StudentPortal />
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/form" element={<FormPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/submitted" element={<SubmittedPage />} />
+        </Route>
+
+        <Route path="/auth/callback" element={<AuthCallback />} />
+
+        <Route element={<PortalLayout />}>
+          <Route
+            path="/student-portal"
+            element={
+              <RequireAuth>
+                <StudentPortal />
+              </RequireAuth>
+            }
+          />
+
+          <Route
+            path="/business-portal"
+            element={
+              <RequireAuth>
+                <BusinessPortal />
+              </RequireAuth>
+            }
+          />
+
+          <Route
+            path="/admin-portal"
+            element={
+              <RequireAuth>
+                <RCPortal />
+              </RequireAuth>
+            }
+          />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </>
   );
 }
